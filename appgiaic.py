cb158b48a15aad99c60696be13af8e91c0a38cb8
Mehak Alamgir <159041130+missmehakalamgir@users.noreply.github.com>
2025-05-01 13:49:24 +0500

    Update appgiaic.py

--- appgiaic.py
+++ appgiaic.py
@@ -199,7 +199,10 @@ if st.button("⚙️ Refactor Now"):  # Refactor button click hone par action
 st.markdown("""
     <div class='footer'>
         RefactorPro &copy; 2025 &mdash; Built with ❤️ by Mehak Alamgir<br>
-        <a href="https://www.linkedin.com" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/7/75/LinkedIn_Logo_2013.svg" alt="LinkedIn"></a>
-        <a href="https://www.twitter.com" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/6/60/Twitter_Logo_2021.svg" alt="Twitter"></a>
+        <div class='social-icons'>
+            <a href="https://github.com/mehakalamgir"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></a>
+            <a href="https://linkedin.com/in/mehakalamgir"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png"></a>
+            <a href="https://youtube.com/@mehakalamgir"><img src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png"></a>
+        </div>
     </div>
 """, unsafe_allow_html=True)
