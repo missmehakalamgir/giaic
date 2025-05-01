be3d519312117f7aecc63b6aee169b470444538f
Mehak Alamgir <159041130+missmehakalamgir@users.noreply.github.com>
2025-05-01 13:49:03 +0500

    Update appgiaic.py

--- appgiaic.py
+++ appgiaic.py
@@ -196,15 +196,10 @@ if st.button("⚙️ Refactor Now"):  # Refactor button click hone par action
                 st.info("No optimization suggestions available.")  # Agar koi suggestion na ho toh info display karna
 
 # Footer with Social Icons
-# Footer with Social Icons
 st.markdown("""
     <div class='footer'>
         RefactorPro &copy; 2025 &mdash; Built with ❤️ by Mehak Alamgir<br>
-        <a href="https://www.linkedin.com" target="_blank">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/7/75/LinkedIn_Logo_2013.svg" alt="LinkedIn" style="width: 30px; height: 30px; margin: 0 10px;">
-        </a>
-        <a href="https://www.twitter.com" target="_blank">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/6/60/Twitter_Logo_2021.svg" alt="Twitter" style="width: 30px; height: 30px; margin: 0 10px;">
-        </a>
+        <a href="https://www.linkedin.com" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/7/75/LinkedIn_Logo_2013.svg" alt="LinkedIn"></a>
+        <a href="https://www.twitter.com" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/6/60/Twitter_Logo_2021.svg" alt="Twitter"></a>
     </div>
 """, unsafe_allow_html=True)
